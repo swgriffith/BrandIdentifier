7b9a3c6db965c2cd2eaa97fa42ff3803b668ff8b
John de Havilland <johndehavilland@gmail.com>
2018-06-10 22:56:07 -0400

    added logic to detect beginning

--- BrandIdentifier/GetAdditionalFrames.cs
+++ BrandIdentifier/GetAdditionalFrames.cs
@@ -13,6 +13,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BrandIdentifier
 {
@@ -31,40 +32,86 @@ namespace BrandIdentifier
 
             GetSettings(context);
 
-            string possition = WebUtility.UrlDecode(req.Query["possition"]);
+            string position = WebUtility.UrlDecode(req.Query["position"]);
             string fileName = WebUtility.UrlDecode(req.Query["filename"]);
             string vidurl = new StreamReader(req.Body).ReadToEnd();
-
+            string start = GetStartTime(position);
             log.Info(Directory.GetCurrentDirectory());
             var psi = new ProcessStartInfo();
             psi.FileName = @".\ffmpeg\ffmpeg.exe";
-            psi.Arguments = $"-i \"{vidurl}\" -ss {possition} -frames:v 15 {fileName}_%d.jpg";
+            psi.Arguments = $"-i \"{vidurl}\" -ss {start} -frames:v 15 {fileName}_%d.jpg";
             psi.RedirectStandardOutput = true;
             psi.RedirectStandardError = true;
             psi.UseShellExecute = false;
 
             log.Info($"Args: {psi.Arguments}");
             var process = Process.Start(psi);
-            log.Info(process.StandardOutput.ReadToEnd());
-            log.Info(process.StandardError.ReadToEnd());
+            //log.Info(process.StandardOutput.ReadToEnd());
+            //log.Info(process.StandardError.ReadToEnd());
             process.WaitForExit((int)TimeSpan.FromSeconds(60).TotalMilliseconds);
 
             List<frame> frameList = new List<frame>();
 
             for (int i = 1; i < 15; i++)
             {
-                ReadFileAndAnalyze($"{fileName}_{i}.jpg");
-                //frame fr = new frame(
+                var result = ReadFileAndAnalyze($"{fileName}_{i}.jpg");
+
+                dynamic customVisionResults = JsonConvert.DeserializeObject(result);
+                if (customVisionResults.predictions != null)
+                {
+                    foreach (var item in customVisionResults.predictions)
+                    {
+                        decimal probability = (decimal)item.probability;
+                        if (probability >= .02m)
+                        {
+                            if (item.tagName == "brand_specific")
+                            {
+                                frameList.Add(new frame
+                                {
+                                    id = i,
+                                    matchProb = probability
+                                });
+                            }
+
+                        }
+
+                    }
+                }
+
+                //log.Info(result);
             }
 
-            if (possition != null && vidurl !=null)
+            var match = frameList.OrderByDescending(x => x.matchProb).First();
+            var newTime = GetNewTime(start, match.id-3);
+
+            if (newTime != null && vidurl !=null)
             {
-                return (ActionResult)new OkObjectResult($"possition={possition} vidurl={vidurl}");
+                return (ActionResult)new OkObjectResult($"position={newTime} vidurl={vidurl}");
             }
             else
             {
-                return new BadRequestObjectResult("Please confirm the input possition and vidurl URL params are set.");
+                return new BadRequestObjectResult("Please confirm the input position and vidurl URL params are set.");
             }
+        }
+
+        private static string GetNewTime(string startPos, int frame)
+        {
+
+            TimeSpan s = TimeSpan.Parse(startPos);
+            double startFrame = (s.TotalSeconds * 25);
+            double seconds = (startFrame + frame) / 25;
+            TimeSpan final = TimeSpan.FromSeconds(seconds);
+            return final.ToString("hh\\:mm\\:ss\\.fff");
+        }
+
+        private static string GetStartTime(string startPos)
+        {
+            TimeSpan s = TimeSpan.Parse(startPos);
+            double startFrame = (s.TotalSeconds * 25) - 15;
+            double seconds = startFrame / 25;
+            TimeSpan final = TimeSpan.FromSeconds(seconds);
+            return final.ToString("hh\\:mm\\:ss\\.fff");
+
         }
 
         private static string ReadFileAndAnalyze(string fileName)
@@ -104,11 +151,9 @@ namespace BrandIdentifier
 
     class frame
     {
-        public string fileName { get; set; }
-        public string timestamp { get; set; }
-        public string framenum { get; set; }
-        public string matchTag { get; set; }
-        public string matchProb { get; set; }
+        public decimal matchProb { get; set; }
+
+        public int id { get; set; }
 
     }
 }
